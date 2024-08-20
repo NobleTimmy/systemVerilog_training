b54f1d588dfbcb8a1f8a2420c3b31fb3aad6d41a
NobleTimmy <GabidullinTK@gmail.com>
2024-08-20 19:43:51 +0300

    Home work#1 ended

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 REPO_ROOT		:= /home/ubuntu/systemVerilog/repos/systemVerilog_training
 PROJECT_NUM		:= 0
-PROJECT_PATH 	?= ${REPO_ROOT}/projects/myProjects${PROJECT_NUM}/homeWorksOfDataTypes/work_1
+PROJECT_PATH 	?= ${REPO_ROOT}/projects/myProjects${PROJECT_NUM}/homeWorksOfDataTypes/
 FILE_NAME_VCD := tb
 
 .PHONY: xrun_comp xrun_sim xrun_wave clear all all_wave create_docs
--- projects/myProjects1/homeWorksOfDataTypes/src/tb.sv
+++ projects/myProjects1/homeWorksOfDataTypes/src/tb.sv
@@ -6,6 +6,13 @@ module tb;
   logic [3:0] my_data;    //Declar a 4-bit logic type variable
   logic     en;         //Declar a 1-bit logic type variable
 
+  bit       var_a;
+  bit [3:0] var_b;
+
+  logic [3:0] x_val;
+  logic [15:0] x_bval;
+
+
   initial begin
       $display ("my_data=0x%0h en=%0b", my_data, en);
       my_data = 4'hB;
@@ -21,6 +28,15 @@ module tb;
       my_data = 4'h6;
       #2;
       $display ("my_data=0x%0b en=%0b", my_data, en);
+
+      $display ("Initial var_a=0%0b var_b=0%0h", var_a, var_b);
+      var_a = 1;
+      var_b = 4'hE;
+      $display ("New values var_a=0%0b var_b=0%0h", var_a, var_b);
+      x_bval = 16'h481a;
+      $display ("Truncated value: x_bval=0%0h", x_bval);
+      var_b = 4'b01zx;
+      $display ("var_b=%b", var_b);
   end
   assign en = my_data[0];
 endmodule:tb
